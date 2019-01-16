9fb3a01afa7b8e174d60e2e2508c86012d0214e9
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2019-01-16 10:39:36 -0500

    Revert "committing new files"
    This reverts commit eea84102a16179f107edddb06162a9c85694197b.

--- src/someproject/SomeProject.java
+++ src/someproject/SomeProject.java
@@ -15,7 +15,6 @@ public class SomeProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Liz");
         // TODO code application logic here
     }
     
